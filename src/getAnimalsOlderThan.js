3e5db2b05e849d7a08aa72600d3fb118fe0a1954
renathogomes <renatho.20gomes@gmail.com>
2023-05-10 11:00:06 -0300

    requisitos 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((especie) => especie.name === animal).residents
+  .map((idade) => idade.age)
+  .every((elemento) => elemento >= age);
 
+console.log(getAnimalsOlderThan('lions', 40));
 module.exports = getAnimalsOlderThan;
