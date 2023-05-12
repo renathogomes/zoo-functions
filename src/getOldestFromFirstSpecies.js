59763c026499a56f95bc54c122dfcf5249b4666b
renathogomes <renatho.20gomes@gmail.com>
2023-05-12 02:54:23 -0300

    nao esta facil

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,12 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  const arrayIds = data.employees.find((elemento) => elemento.id === id).responsibleFor;
-  const elementoSpecies = data.species.filter((elemento) => elemento.id);
 
 };
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
 
-// quero comparar o elemento atual e trazer o objeto se tiver a idade maior
+/* const arrayIds = data.employees.find((elemento) => elemento.id === id).responsibleFor;
+const elementoSpecies = data.species.filter((elemento) => elemento.id);
+ */// quero comparar o elemento atual e trazer o objeto se tiver a idade maior
