4a030c99fec2fb9e1981843341ffd7a209608868
renathogomes <renatho.20gomes@gmail.com>
2023-05-12 10:52:48 -0300

    3 feito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,16 @@
-/* const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-const isManager = () => {
+const getEmployeeByName = (parametro) => data.employees.reduce((acumulado, atual) => {
+  if (atual.firstName === parametro || atual.lastName === parametro) {
+    return atual;
+  }
+  return acumulado;
+}, {});
 
-};
-
-isManager();
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
- */
+
+// buscar por pessoas colaboradoras através de seu primeiro ou último nome
+// Retorne um objeto vazio caso a função não receba parâmetros
+// Retorne as informações da pessoa colaboradora caso o parâmetro seja igual ao nome ou igual ao último nome
