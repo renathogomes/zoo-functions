56cd6b50de3c1a3b3a5ac35b4ae8a5dd26dff9a3
renathogomes <renatho.20gomes@gmail.com>
2023-05-12 17:01:01 -0300

    4 feito  linter

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  const { species } = data;
-  return species.find(() => {});
+
 };
 
 console.log(countAnimals({ species: 'penguins' }));
