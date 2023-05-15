a80d6b7785c3be7c9693c6bf28aa82d4d2d78fd8
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 18:25:11 -0300

    12 feito

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,10 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('verifica se o zoo esta fechado', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('verifica se o zoo esta aberto', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+});
