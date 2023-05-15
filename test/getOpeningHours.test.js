98df4167845665911c7066a4b9287963df041166
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 18:57:52 -0300

    16 feito

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -22,4 +22,10 @@ describe('Testes da função getOpeningHours', () => {
   it('verifica se AM ou PM existem', () => {
     expect(getOpeningHours(!['AM', 'PM'])).toBe('The abbreviation must be \'AM\' or \'PM\'');
   });
+  it('verifica se os minutos são limitados de 0 a 59', () => {
+    expect(getOpeningHours('Tuesday', '09:60-AM')).toBe('The minutes must be between 0 and 59');
+  });
+  it('verifica se as horas são limitadas de 0 a 12', () => {
+    expect(getOpeningHours('Monday', '13:00-AM')).toBe('The hour must be between 0 and 12');
+  });
 });
