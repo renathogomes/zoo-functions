62294e1924d478bafb6d6e9ba6e6f5e2b3908aa1
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 11:52:08 -0300

    ainda na 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -4,4 +4,10 @@ describe('Testes da função HandlerElephants', () => {
   it('retorna uma quantidade de elefantes', () => {
     expect(handlerElephants('count', 'elephants')).toBe(4);
   });
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names', 'elephants')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge', 'elephants')).toBe(10.5);
+  });
 });
