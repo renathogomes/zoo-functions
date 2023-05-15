8e83b3cce80d66afcf0eb8764556506b92904d88
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 18:41:32 -0300

    13 feito

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -7,4 +7,19 @@ describe('Testes da função getOpeningHours', () => {
   it('verifica se o zoo esta aberto', () => {
     expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
   });
+  it('verifica se o zoo esta fechado, nas quartas', () => {
+    expect(getOpeningHours('Wednesday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  it('verifica se o dia é válido', () => {
+    expect(getOpeningHours('Thu', '09:00-AM')).toBe('The day must be valid. Example: Monday');
+  });
+  it('verifica se o PM/AM é válido', () => {
+    expect(getOpeningHours('Friday', '09:00-ZM')).toBe('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('verifica se a dezena da hora é válida', () => {
+    expect(getOpeningHours('Saturday', 'C9:00-AM')).toBe('The hour should represent a number');
+  });
+  it('verifica se AM ou PM existem', () => {
+    expect(getOpeningHours(!['AM', 'PM'])).toBe('The abbreviation must be \'AM\' or \'PM\'');
+  });
 });
