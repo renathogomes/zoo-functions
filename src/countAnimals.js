b986acd12ad4d70b0f57132ea976910b34f6ee39
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 11:19:10 -0300

    5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,10 +1,23 @@
-/* const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-
+  if (!animal) {
+    const objetoV = {};
+    data.species.forEach((elemento) => {
+      objetoV[elemento.name] = elemento.residents.length;
+    });
+    return objetoV;
+  }
+  const especieSelecionada = animal.species;
+  const objetoEspecie = data.species.find((nome) => nome.name === especieSelecionada);
+  const { residents } = objetoEspecie;
+  if (!animal.sex) {
+    const quantidadeResidentes = residents.length;
+    return quantidadeResidentes;
+  }
+  return residents.filter((el) => el.sex === 'female').length;
 };
 
-console.log(countAnimals({ species: 'penguins' }));
+console.log(countAnimals());
 
 module.exports = countAnimals;
- */
