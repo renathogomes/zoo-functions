7840a3a7701e9c64483139231277379fa4d05471
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 11:39:38 -0300

    6 feito

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna uma quantidade de elefantes', () => {
+    expect(handlerElephants('count', 'elephants')).toBe(4);
+  });
+});
