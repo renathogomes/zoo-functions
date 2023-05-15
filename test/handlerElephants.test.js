ce9ee3666a0207bd79fc9a875741d5c98b2edf1d
renathogomes <renatho.20gomes@gmail.com>
2023-05-15 12:07:33 -0300

    7 feito

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -10,4 +10,16 @@ describe('Testes da função HandlerElephants', () => {
   it('retorna a média de idade dos elefantes', () => {
     expect(handlerElephants('averageAge', 'elephants')).toBe(10.5);
   });
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability', 'elephants')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity', 'elephants')).toBe(5);
+  });
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location', 'elephants')).toBe('NW');
+  });
+  it('não passando parametro, deve retornar underfined', () => {
+    expect(handlerElephants()).toBeUnderfined();
+  });
 });
