dcfb4082033e12663d5b7b580d3a4c416ed696f8
renathogomes <renatho.20gomes@gmail.com>
2023-05-16 15:11:44 -0300

    11 feitooo

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,21 +1,7 @@
 const data = require('../data/zoo_data');
 
-const { species, employees } = data;
-
-const verificaDias = (nome) => species
-  .find((animal) => animal.name === nome).availability;
-
-console.log(verificaDias('lions'));
-
 const getSchedule = (scheduleTarget) => {
-  if (!scheduleTarget) {
-    return 'alguma coisa deve acontecer';
-  }
-  if (verificaDias()) {
-    verificaDias(scheduleTarget);
-  }
+  // seu código aqui
 };
-
-console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
