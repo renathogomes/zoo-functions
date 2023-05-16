a0cb82ed951b6c6c897847f2943661da789613b1
renathogomes <renatho.20gomes@gmail.com>
2023-05-16 12:03:41 -0300

    so pra salvar

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,66 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getSpecies = (pessoa) => data.species
+  .filter((specie) => pessoa.responsibleFor.includes(specie.id));
+
+const getEmployees = () => data.employees.map((pessoa) => ({
+  id: pessoa.id,
+  fullName: `${pessoa.firstName} ${pessoa.lastName}`,
+  species: getSpecies(pessoa).map((specie) => specie.name),
+  locations: getSpecies(pessoa).map((specie) => specie.location),
+}));
+
+const getId = (idx) => data.employees
+  .filter((pessoa) => pessoa.id.includes(idx))
+  .map((achado) => ({
+    id: achado.id,
+    fullName: `${achado.firstName} ${achado.lastName}`,
+    species: getSpecies(achado).map((specie) => specie.name),
+    locations: getSpecies(achado).map((specie) => specie.location),
+  }))
+  .find((elemento) => elemento);
+
+const getName = (name) => data.employees
+  .filter((pessoaName) => pessoaName.firstName.includes(name))
+  .map((nome) => ({
+    id: nome.id,
+    fullName: `${nome.firstName} ${nome.lastName}`,
+    species: getSpecies(nome).map((specie) => specie.name),
+    locations: getSpecies(nome).map((specie) => specie.location),
+  }))
+  .find((elemento) => elemento);
+
+const getSobrenome = (name) => data.employees
+  .filter((pessoaSobrenome) => pessoaSobrenome.lastName.includes(name))
+  .map((sobrenome) => ({
+    id: sobrenome.id,
+    fullName: `${sobrenome.firstName} ${sobrenome.lastName}`,
+    species: getSpecies(sobrenome).map((specie) => specie.name),
+    locations: getSpecies(sobrenome).map((specie) => specie.location),
+  }))
+  .find((elemento) => elemento);
+
+const verificaNome = (nomee) => {
+  if (getName(nomee)) {
+    return getName(nomee);
+  }
+  if (getSobrenome(nomee)) {
+    return getSobrenome(nomee);
+  }
+  throw new Error('Informações inválidas');
 };
 
+const getEmployeesCoverage = (parametro) => {
+  if (!parametro) {
+    return getEmployees();
+  }
+  if (parametro.id) {
+    return getId(parametro.id);
+  }
+  if (parametro.name) {
+    return verificaNome(parametro.name);
+  }
+};
+
+console.log(getEmployeesCoverage({ name: 'renatho' }));
 module.exports = getEmployeesCoverage;
